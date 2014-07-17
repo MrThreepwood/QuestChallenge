45e55266c8b480960a87ac4b03ab0a834fded23e
teebird <tee.bird@comcast.net>
2014-07-17 12:17:38 -0500

    beggining of list implementation+ fragment swap method, untested

--- app/src/main/java/com/coopinc/questchallenge/app/MainActivity.java
+++ app/src/main/java/com/coopinc/questchallenge/app/MainActivity.java
@@ -1,18 +1,22 @@
 package com.coopinc.questchallenge.app;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    View mainContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.container_main);
         if (findViewById(R.id.main_container) != null) {
+            mainContainer = findViewById(R.id.main_container);
             LoginFragment login = new LoginFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.main_container, login).commit();
         }
@@ -33,5 +37,11 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void fragmentSwap(android.support.v4.app.Fragment current, android.support.v4.app.Fragment next) {
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(mainContainer.getId(), next);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
